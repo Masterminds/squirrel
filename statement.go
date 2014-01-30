c30293b7e28c2888291893ed2a40931a5ba102a8
Lann Martin <lmartin@revolv.com>
2014-01-30 13:31:32 -0700

    Expose StatementBuilder type as StatementBuilderType

--- statement.go
+++ statement.go
@@ -2,22 +2,21 @@ package squirrel
 
 import "github.com/lann/builder"
 
-type statementBuilder builder.Builder
+// StatementBuilderType is the type of StatementBuilder.
+type StatementBuilderType builder.Builder
 
-func (b statementBuilder) Select(columns ...string) SelectBuilder {
+// Select returns a SelectBuilder for this StatementBuilderType.
+func (b StatementBuilderType) Select(columns ...string) SelectBuilder {
 	return SelectBuilder(b).Columns(columns...)
 }
 
-func (b statementBuilder) RunWith(runner Runner) statementBuilder {
-	return builder.Set(b, "RunWith", runner).(statementBuilder)
+// RunWith sets the RunWith field for any child builders.
+func (b StatementBuilderType) RunWith(runner Runner) StatementBuilderType {
+	return builder.Set(b, "RunWith", runner).(StatementBuilderType)
 }
 
-// StatementBuilder is a parent builder for other statement builders.
-//
-// Currently StatementBuilder has only two methods: RunWith, which has the same
-// semantics as SelectBuilder.RunWith, and Select, which returns a SelectBuilder
-// with RunWith already set.
-var StatementBuilder = statementBuilder(builder.EmptyBuilder)
+// StatementBuilder is a parent builder for other builders, e.g. SelectBuilder.
+var StatementBuilder = StatementBuilderType(builder.EmptyBuilder)
 
 // Select returns a new SelectBuilder, optionally setting some result columns.
 //
