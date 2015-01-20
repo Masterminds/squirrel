b44e999c45073da0753ca24e14e7865e6d04ddca
elgris <iddqd07@yandex.ru>
2015-01-20 12:50:11 +0700

    Added tests for case statement. 'AS' expression is needed though

---
+++ case_test.go
@@ -0,0 +1,78 @@
+package squirrel
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCaseWithVal(t *testing.T) {
+	caseStmt := Case().
+		What("number").
+		When("1", "one").
+		When("2", "two").
+		Else(Expr("?", "big number"))
+
+	qb := Select().
+		Column(caseStmt).
+		From("table")
+	sql, args, err := qb.ToSql()
+
+	assert.NoError(t, err)
+
+	expectedSql := "SELECT CASE number " +
+		"WHEN 1 THEN one " +
+		"WHEN 2 THEN two " +
+		"ELSE ? " +
+		"END " +
+		"FROM table"
+	assert.Equal(t, expectedSql, sql)
+
+	expectedArgs := []interface{}{"big number"}
+	assert.Equal(t, expectedArgs, args)
+}
+
+func TestCaseWithNoVal(t *testing.T) {
+	caseStmt := Case().
+		When(Eq{"x": 0}, "x is zero").
+		When(Expr("x > ?", 1), Expr("CONCAT('x is greater than ', ?)", 2))
+
+	qb := Select().Column(caseStmt).From("table")
+	sql, args, err := qb.ToSql()
+
+	assert.NoError(t, err)
+
+	expectedSql := "SELECT CASE " +
+		"WHEN x = ? THEN x is zero " +
+		"WHEN x > ? THEN CONCAT('x is greater than ', ?) " +
+		"END " +
+		"FROM table"
+
+	assert.Equal(t, expectedSql, sql)
+
+	expectedArgs := []interface{}{0, 1, 2}
+	assert.Equal(t, expectedArgs, args)
+}
+
+func TestCaseWithExpr(t *testing.T) {
+	caseStmt := Case().
+		What(Expr("x = ?", true)).
+		When("true", Expr("?", "it's true!")).
+		Else("42")
+
+	qb := Select().Column(caseStmt).From("table")
+	sql, args, err := qb.ToSql()
+
+	assert.NoError(t, err)
+
+	expectedSql := "SELECT CASE x = ? " +
+		"WHEN true THEN ? " +
+		"ELSE 42 " +
+		"END " +
+		"FROM table"
+
+	assert.Equal(t, expectedSql, sql)
+
+	expectedArgs := []interface{}{true, "it's true!"}
+	assert.Equal(t, expectedArgs, args)
+}
