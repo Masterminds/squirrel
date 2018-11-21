77beef6a92e85fae1769fa334bac2bb6bac96799
sivagollapalli <sivagollapalli88@gmail.com>
2018-11-21 21:56:43 +0530

    Renamed test case statement

--- select_test.go
+++ select_test.go
@@ -166,7 +166,7 @@ func TestSelectWithOptions(t *testing.T) {
 	assert.Equal(t, "SELECT DISTINCT SQL_NO_CACHE * FROM foo", sql)
 }
 
-func TestSelectWithLimitALL(t *testing.T) {
+func TestSelectWithRemoveLimit(t *testing.T) {
 	sql, _, err := Select("*").From("foo").Limit(10).RemoveLimit().ToSql()
 
 	assert.NoError(t, err)
