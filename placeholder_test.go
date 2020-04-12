8186d410ca05a412eb6b004639ff2d08b909d9a4
Pavel Patrin <pavelpatrin@gmail.com>
2020-04-12 17:41:11 +0300

    MS SQL Server compatible placeholders (#244)
    * Added at placeholders (@p1) syntax. This usable with MS SQL Server.
    Without this feature impossible to use squirrel with SQL Server.
    
    * Fixed debugPlaceholder()
    
    * Renamed Atp to AtP.
    
    Co-authored-by: Pavel Patrin <ppatrin@nvidia.com>

--- placeholder_test.go
+++ placeholder_test.go
@@ -25,6 +25,12 @@ func TestColon(t *testing.T) {
 	assert.Equal(t, "x = :1 AND y = :2", s)
 }
 
+func TestAtp(t *testing.T) {
+	sql := "x = ? AND y = ?"
+	s, _ := AtP.ReplacePlaceholders(sql)
+	assert.Equal(t, "x = @p1 AND y = @p2", s)
+}
+
 func TestPlaceholders(t *testing.T) {
 	assert.Equal(t, Placeholders(2), "?,?")
 }
@@ -39,6 +45,12 @@ func TestEscapeColon(t *testing.T) {
 	sql := "SELECT uuid, \"data\" #> '{tags}' AS tags FROM nodes WHERE  \"data\" -> 'tags' ??| array['?'] AND enabled = ?"
 	s, _ := Colon.ReplacePlaceholders(sql)
 	assert.Equal(t, "SELECT uuid, \"data\" #> '{tags}' AS tags FROM nodes WHERE  \"data\" -> 'tags' ?| array[':1'] AND enabled = :2", s)
+}
+
+func TestEscapeAtp(t *testing.T) {
+	sql := "SELECT uuid, \"data\" #> '{tags}' AS tags FROM nodes WHERE  \"data\" -> 'tags' ??| array['?'] AND enabled = ?"
+	s, _ := AtP.ReplacePlaceholders(sql)
+	assert.Equal(t, "SELECT uuid, \"data\" #> '{tags}' AS tags FROM nodes WHERE  \"data\" -> 'tags' ?| array['@p1'] AND enabled = @p2", s)
 }
 
 func BenchmarkPlaceholdersArray(b *testing.B) {
